e4dc79932bd3663eeeefe4b75bd0a597ff6ececa
Emma <eeg4@uw.edu>
2022-06-20 16:16:11 -0700

    PalindromeNumber

--- TwoSum.js
+++ TwoSum.js
@@ -1,5 +1,4 @@
 //Approach 1
-/*Time Complexity and Space Complexity Unknown*/
 var TwoSum = function (nums, target) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = (i + 1); j < nums.length; j++) {
@@ -13,11 +12,12 @@ var TwoSum = function (nums, target) {
 var tar = 17;
 var arr = [1, 3, 7, 9, 22, -5]
 TwoSum(arr, tar)
+/*The Time Complexity of Approach 1 is O(n^2). For each element, we try to find its complement by looping through the rest of the array which takes O(n^2) time. 
+Therefore, the time complexity is O(n^2).*/
+/*The Space Complexity of Approach 1 is O(1), constant space. The space required does not depend on the size of the input array, so only constant space is used.*/
 
 
 //Approach 2. 
-/*Runtime: 48 ms, faster than 98.37% of JavaScript online submissions for Two Sum.
-Memory Usage: 34 MB, less than 96.69% of JavaScript online submissions for Two Sum.*/
 var TwoSum = function (nums, target) {
     let dic = {}
     for (let i = 0; i < nums.length; i++) {
@@ -34,3 +34,5 @@ var TwoSum = function (nums, target) {
 var tar = 17;
 var arr = [1, 3, 7, 9, 22, -5]
 TwoSum(arr, tar)
+/*The Time Complexity of Approach 2 is ?*/
+/*The Space Complexity of Approach 1 is ?*/
