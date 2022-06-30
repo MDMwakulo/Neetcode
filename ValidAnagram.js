682b2424461a2f76fa9bc8a5ae3a94909636a26a
Emma <eeg4@uw.edu>
2022-06-29 20:32:17 -0700

    GroupAnagrams

--- ValidAnagram.js
+++ ValidAnagram.js
@@ -9,8 +9,8 @@ var isAnagram = function (s, t) {
     } else { console.log(false) }
 
 };
-/*The Time Complexity of Approach 1 is */
-/*The Space Complexity of Approach 1 is */
+/*The Time Complexity of Approach 1 is ? */
+/*The Space Complexity of Approach 1 is ? */
 
 
 //Approach 2
@@ -20,7 +20,7 @@ function isAnagram(s, t) {
     t.split('').map(c => map[c] = map[c] ? map[c] - 1 : -1);
     return Object.keys(map).every(k => map[k] === 0);
 }
-/*The Time Complexity of Approach 1 is */
-/*The Space Complexity of Approach 1 is */
+/*The Time Complexity of Approach 1 is ? */
+/*The Space Complexity of Approach 1 is ? */
 
 isAnagram("anagram", "nagaram")
